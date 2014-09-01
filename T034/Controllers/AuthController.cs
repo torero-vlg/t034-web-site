cd7f9629086eff5d764acdfd66e82341c91ff198
artem.v.rusanov <artem.v.rusanov@gmail.com>
2014-09-01 18:56:11 +0000

    авторизация через яндекс

---
+++ T034/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using T034.Models;
+using T034.Tools;
+
+namespace T034.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult Login()
+        {
+            var client_id = "030edcedc0264dc188a18f4779642970";//код приложения
+            var client_secret = "8f71a3459a104af9a9e05e52af8b03cd";//пароль приложения
+            var code = Request.QueryString["code"];
+
+            var stream = HttpTools.PostStream("https://oauth.yandex.ru/token", string.Format("grant_type=authorization_code&code={0}&client_id={1}&client_secret={2}", code, client_id, client_secret));
+
+            var model = SerializeTools.Deserialize<TokenModel>(stream);
+
+            var userCookie = new HttpCookie("user_token", model.access_token);
+            userCookie.Expires.AddDays(30);
+            HttpContext.Response.Cookies.Set(userCookie);
+
+            return RedirectToActionPermanent("Index", "Home");
+        }
+
+        public ActionResult Logout()
+        {
+            if (Request.Cookies["user_token"] != null)
+            {
+                var user = new HttpCookie("user_token")
+                {
+                    Expires = DateTime.Now.AddDays(-1),
+                    Value = null
+                };
+                Response.Cookies.Remove("user_token");
+            }
+            Response.Cookies.Remove("user_token");
+            return RedirectToActionPermanent("Index", "Home");
+        }
+    }
+}
--- T034/Controllers/NewsController.cs
+++ T034/Controllers/NewsController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using Db.Entity;
 using Db.Entity.Administration;
+using T034.Models;
+using T034.Tools;
 
 namespace T034.Controllers
 {
@@ -21,6 +25,7 @@ namespace T034.Controllers
         }
 
         [HttpGet]
+        [AuthorizeUser]
         public ActionResult Add()
         {
             return View();
@@ -28,11 +33,31 @@ namespace T034.Controllers
 
         public ActionResult Add(News news)
         {
-            news.LogDate = DateTime.Now;
-            news.User = new User { Id = 1 };
+            var stream = HttpTools.PostStream("https://login.yandex.ru/info",
+                                              string.Format("oauth_token={0}", Request.Cookies["user_token"].Value));
+            var user = SerializeTools.Deserialize<UserModel>(stream);
 
-            var result = MvcApplication.Db.Save(news);
-            return RedirectToAction("Single", new {newsId = result});
+            //найдём пользователя в БД
+            var list = MvcApplication.Db.Where<User>(u => u.Login == user.display_name);
+            if (list.Any())
+            {
+                news.LogDate = DateTime.Now;
+                news.User = new User {Id = list.FirstOrDefault().Id};
+
+                var result = MvcApplication.Db.Save(news);
+                return RedirectToAction("Single", new {newsId = result});
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+
+    public class AuthorizeUserAttribute : AuthorizeAttribute 
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Request.Cookies["user_token"] != null)
+                return true;
+            return false;
         }
     }
 }
