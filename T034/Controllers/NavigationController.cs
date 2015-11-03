5368ba672215bc26039765473020bd08da8dfb44
torero-vlg <artem.v.rusanov@gmail.com>
2015-11-04 00:46:58 +0300

    Главное меню берётся из БД

--- T034/Controllers/NavigationController.cs
+++ T034/Controllers/NavigationController.cs
@@ -1,12 +1,29 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using AutoMapper;
+using Db.Entity;
+using T034.ViewModel;
 
 namespace T034.Controllers
 {
-    public class NavigationController : Controller
+    public class NavigationController : BaseController
     {
         public ActionResult MainMenu()
         {
-            return PartialView();
+            try
+            {
+                var items = Db.Select<MenuItem>();
+
+                var model = new List<MenuItemViewModel>();
+                model = Mapper.Map(items, model);
+
+                return PartialView(model);
+            }
+            catch (Exception ex)
+            {
+                return PartialView("ErrorMessage", (object)"Получение списка");
+            }
         }
 
         public ActionResult ManagementMenu()
