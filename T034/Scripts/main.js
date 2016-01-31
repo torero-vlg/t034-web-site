5badd13395469a39ff4073a330fdd9d884a51730
Артём <artem.v.rusanov@gmail.com>
2016-01-31 11:18:53 +0300

    подключил RequireJS, осталось настроить модули на каждой странице

---
+++ T034/Scripts/main.js
@@ -0,0 +1,15 @@
+﻿require.config({
+    baseUrl: "/Scripts/app",
+    paths: {
+        jquery: "../lib/jquery-1.9.0",
+        jqueryValidate: "../lib/jquery.validate",
+        jqueryValidateUnobtrusive: "../lib/jquery.validate.unobtrusive",
+        bootstrap: "../lib/bootstrap"
+    },
+    shim: {
+        jqueryValidate: ["jquery"],
+        jqueryValidateUnobtrusive: ["jquery", "jqueryValidate"]
+    }
+});
+
+require();
