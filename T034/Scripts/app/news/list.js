5bd5561c9022c4e64aa9bb75585116685bf0cde0
Артём <artem.v.rusanov@gmail.com>
2016-01-31 21:20:03 +0300

    RequiredJS подключен, но не работает модуль News/List

---
+++ T034/Scripts/app/news/list.js
@@ -0,0 +1,16 @@
+﻿define('jqueryValidate', function (dataTables) {
+
+    alert();
+
+    //$('#table_id').DataTable();
+    ////{
+    ////    'language': {
+    ////        'url': '@Url.Content("~/Content/DataTables-1.10.2/dataTables.russian.langs")"
+    ////    }
+    ////});
+
+    return {
+        Initialize: function () {
+        }
+    }
+});
--- T034/Scripts/main.js
+++ T034/Scripts/main.js
@@ -1,15 +1,20 @@
 ﻿require.config({
-    baseUrl: "/Scripts/app",
+    baseUrl: '/Scripts',
     paths: {
-        jquery: "../lib/jquery-1.9.0",
-        jqueryValidate: "../lib/jquery.validate",
-        jqueryValidateUnobtrusive: "../lib/jquery.validate.unobtrusive",
-        bootstrap: "../lib/bootstrap"
+        main: 'main',
+        jquery: 'lib/jquery-1.9.0',
+        jqueryui: 'lib/jquery-ui-1.9.0',
+        jqueryValidate: 'jquery.validate',
+        jqueryValidateUnobtrusive: 'lib/jquery.validate.unobtrusive',
+        bootstrap: 'lib/bootstrap',
+        dataTables: 'lib/DataTables-1.10.2/media/js/jquery.dataTables',
+        ckeditor: 'lib/ckeditor'
     },
     shim: {
-        jqueryValidate: ["jquery"],
-        jqueryValidateUnobtrusive: ["jquery", "jqueryValidate"]
+        jqueryui: ['jquery'],
+        bootstrap: ['jquery'],
+        jqueryValidate: ['jquery'],
+        jqueryValidateUnobtrusive: ['jquery', 'jqueryValidate']
     }
 });
 
-require();
