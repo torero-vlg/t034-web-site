900fe37ede17378154d1ececfedc5ef7bdf44166
Артём <artem.v.rusanov@gmail.com>
2016-02-04 19:33:49 +0300

    общий файл инициализации datatables t034.datatables.js

--- T034/Scripts/main.js
+++ T034/Scripts/main.js
@@ -8,6 +8,7 @@
         jqueryValidateUnobtrusive: 'lib/jquery.validate.unobtrusive',//заглавные буквы не работают
         bootstrap: 'lib/bootstrap',
         datatables: 'lib/DataTables-1.10.2/media/js/jquery.dataTables',
+        initdatatables: 'app/t034/t034.dataTables',
         ckeditor: 'lib/ckeditor/ckeditor',
         ckeditoradapter: 'lib/ckeditor/adapters/jquery'
     },
@@ -20,6 +21,9 @@
         },
         datatables: {
             deps: ['jquery']
+        },
+        initdatatables: {
+            deps: ['datatables']
         },
         jqueryValidate: {
             deps: ['jquery']
