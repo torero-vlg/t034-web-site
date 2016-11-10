e9c859c10eb4055f3307c3f9712820a8104fa8de
Артём <artem.v.rusanov@gmail.com>
2016-11-10 19:04:33 +0300

    контроллер стилей

---
+++ T034/Controllers/StyleController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using Db.Entity.Administration;
+using OAuth2;
+using T034.ViewModel;
+
+namespace T034.Controllers
+{
+    public class StyleController : BaseController
+    {
+        public StyleController(AuthorizationRoot authorizationRoot) : base(authorizationRoot)
+        {
+        }
+        
+        public ActionResult LowVision(string styleName)
+        {
+            var userCookie = new HttpCookie("style")
+            {
+                Value = styleName,
+                Expires = DateTime.Now.AddDays(30)
+            };
+            Response.Cookies.Set(userCookie);
+
+            
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
