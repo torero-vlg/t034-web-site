3dd87879730c9480bee9540036a42a0033bc5df8
Артём <artem.v.rusanov@gmail.com>
2016-11-11 20:27:37 +0300

    выбор стиля

--- T034/Scripts/app/layout.js
+++ T034/Scripts/app/layout.js
@@ -1,22 +1,11 @@
 ﻿define([], function () {
 
-    function setStyle(styleName) {
-        $.ajax({
-            data: { styleName: styleName },
-            url: "/Style/Set"
-        });
-    }
+
 
     return {
         Initialize: function () {
 
-            $("#lowVisionButton").click(function () {
-                setStyle("LowVision");
-            });
 
-            $("#generalStyleButton").click(function () {
-                setStyle("General");
-            });
         }
     }
 });
