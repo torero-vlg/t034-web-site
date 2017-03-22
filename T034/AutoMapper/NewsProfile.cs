462a6c0377c7eb448ddebace4d20087aedce4a52
Артём <artem.v.rusanov@gmail.com>
2017-03-22 21:52:18 +0300

    CRUD новостной ленты, при добавлении новости добавлена возможность указывать ленту. Новостной ленте можно указывать пункт меню

--- T034/AutoMapper/NewsProfile.cs
+++ T034/AutoMapper/NewsProfile.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using AutoMapper;
+using Db.Dto;
 using Db.Entity;
 using Db.Entity.Administration;
 using T034.ViewModel;
@@ -10,16 +11,26 @@ namespace T034.AutoMapper
     {
         protected override void Configure()
         {
+            //это должно убраться отсюда
+            //========
             Mapper.CreateMap<News, NewsViewModel>()
                   .ForMember(dest => dest.Body, opt => opt.MapFrom(src => HttpUtility.HtmlDecode(src.Body)))
                   .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.User.Id))
-                  .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.Name));
+                  .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.Name))
+                  .ForMember(dest => dest.NewslineId, opt => opt.MapFrom(src => src.Newsline.Id))
+                  .ForMember(dest => dest.Newsline, opt => opt.MapFrom(src => src.Newsline.Name));
 
             Mapper.CreateMap<NewsViewModel, News>()
                 //.ForMember(dest => dest.Body, opt => opt.MapFrom(src => AutoMapperWebConfiguration.GetSafeHtml(src.Body)))
-                .ForMember(dest => dest.User, opt => opt.MapFrom(src => new User { Id = src.UserId }));
+                .ForMember(dest => dest.User, opt => opt.MapFrom(src => new User { Id = src.UserId }))
+                .ForMember(dest => dest.Newsline, opt => opt.MapFrom(src => new Newsline { Id = src.NewslineId }));
+            //=========
+
+
+
+
+            Mapper.CreateMap<NewsDto, NewsViewModel>();
+            Mapper.CreateMap<NewsViewModel, NewsDto>();
         }
-
-
     }
 }
---
+++ T034/AutoMapper/NewslineProfile.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using AutoMapper;
+using Db.Dto;
+using T034.ViewModel;
+
+namespace T034.AutoMapper
+{
+    public class NewslineProfile : Profile
+    {
+        protected override void Configure()
+        {
+            Mapper.CreateMap<NewslineDto, NewslineViewModel>();
+            Mapper.CreateMap<NewslineViewModel, NewslineDto>();
+
+            Mapper.CreateMap<NewslineDto, SelectListItem>()
+                .ForMember(dest => dest.Value, opt => opt.MapFrom(src => src.Id.ToString()))
+                .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Name));
+        }
+    }
+}
