9e6013b4f7b0ed7984a2820c490f1ac3a10f6f77
Artem <artem.v.rusanov@gmail.com>
2022-01-29 12:19:58 +0300

    t-33 upgrade AutoMapper to 5.2.0, upgrade .NET Framework to 4.8

--- T034.Db/Profiles/AutoMapperConfiguration.cs
+++ T034.Db/Profiles/AutoMapperConfiguration.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Web;
+﻿using System.Reflection;
 using AutoMapper;
 
 namespace Db.Profiles
@@ -43,34 +38,4 @@ namespace Db.Profiles
             }
         }
     }
-
-    //public static class AutoMapperConfiguration
-    //{
-    //    public static List<T> StringToCollection<T>(string ids) where T : Entity.Entity, new()
-    //    {
-    //        return string.IsNullOrEmpty(ids) ? null : new List<T>(ids.Split(new string[] { "," }, StringSplitOptions.None).Select(n => new T { Id = Convert.ToInt32(n) }));
-    //    }
-
-    //    public static string IdsToString<T>(ICollection<T> collection) where T : Entity.Entity
-    //    {
-    //        return collection == null || !collection.Any() ? "" : collection.Select(n => n.Id.ToString()).Aggregate((i, j) => i.ToString() + "," + j.ToString());
-    //    }
-
-    //    public static void Configure(HttpServerUtility server)
-    //    {
-    //        foreach (var profile in Assembly.GetAssembly(typeof(AutoMapperConfiguration)).GetTypes().Where(t => typeof(Profile).IsAssignableFrom(t)))
-    //        {
-    //            Mapper.AddProfile(Activator.CreateInstance(profile) as Profile);
-    //        }
-    //    }
-
-    //    public static string GetSafeHtml(string htmlInputTxt)
-    //    {
-    //        var sb = new StringBuilder(HttpUtility.HtmlEncode(htmlInputTxt));
-
-    //        sb.Replace("&lt;script&gt;", "");
-    //        sb.Replace("&lt;/script&gt;", "");
-    //        return sb.ToString();
-    //    }
-    //}
 }
