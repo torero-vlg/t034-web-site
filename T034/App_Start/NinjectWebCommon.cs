584b8cb3b1a73203b22351e96809e6f9da0bb02e
Artem <artem.v.rusanov@gmail.com>
2022-01-30 22:26:37 +0300

    t-29 move ninject bindings to program.cs

--- T034/App_Start/NinjectWebCommon.cs
+++ T034/App_Start/NinjectWebCommon.cs
@@ -6,32 +6,28 @@ using T034.Core.Services;
 using OAuth2;
 using T034.Repository;
 
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(T034.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(T034.App_Start.NinjectWebCommon), "Stop")]
+//[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(T034.App_Start.NinjectWebCommon), "Start")]
+//[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(T034.App_Start.NinjectWebCommon), "Stop")]
 
 namespace T034.App_Start
 {
     using System;
-    using System.Web;
-
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
     using Ninject;
     using Ninject.Web.Common;
     using Ninject.Web.Common.WebHost;
 
     public static class NinjectWebCommon 
     {
-        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+      //  private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
         public static void Start() 
         {
-            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
-            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
-            bootstrapper.Initialize(CreateKernel);
+            //DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            //DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            //bootstrapper.Initialize(CreateKernel);
         }
         
         /// <summary>
@@ -39,7 +35,7 @@ namespace T034.App_Start
         /// </summary>
         public static void Stop()
         {
-            bootstrapper.ShutDown();
+     //       bootstrapper.ShutDown();
         }
         
         /// <summary>
@@ -51,8 +47,8 @@ namespace T034.App_Start
             var kernel = new StandardKernel();
             try
             {
-                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
-                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+                //kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                //kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
                 RegisterServices(kernel);
                 return kernel;
--- T034/Program.cs
+++ T034/Program.cs
@@ -3,14 +3,19 @@
 // This file may need updated according to the specific scenario of the application being upgraded.
 // For more information on ASP.NET Core hosting, see https://docs.microsoft.com/aspnet/core/fundamentals/host/web-host
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Ninject;
+using Ninject.Web.AspNetCore;
+using Ninject.Web.AspNetCore.Hosting;
+using Ninject.Web.Common;
+using Ninject.Web.Common.SelfHost;
+using OAuth2;
+using T034.Core;
+using T034.Core.Api;
+using T034.Core.DataAccess;
+using T034.Core.Services;
+using T034.Repository;
 
 namespace T034
 {
@@ -19,6 +24,14 @@ namespace T034
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            var hostConfiguration = new AspNetCoreHostConfiguration(args)
+                .UseStartup<Startup>()
+                .UseKestrel()
+                .BlockOnStart();
+
+            var host = new NinjectSelfHostBootstrapper(CreateKernel, hostConfiguration);
+            host.Start();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -27,5 +40,47 @@ namespace T034
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static IKernel CreateKernel()
+        {
+            var settings = new NinjectSettings();
+            // Unfortunately, in .NET Core projects, referenced NuGet assemblies are not copied to the output directory
+            // in a normal build which means that the automatic extension loading does not work _reliably_ and it is
+            // much more reasonable to not rely on that and load everything explicitly.
+            settings.LoadExtensions = false;
+
+            var kernel = new AspNetCoreKernel(settings);
+
+            RegisterServices(kernel);
+
+
+            kernel.Load(typeof(AspNetCoreHostConfiguration).Assembly);
+
+            return kernel;
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<IBaseDb>().ToMethod(c => new NhDbFactory(ConnectionString).CreateBaseDb());
+            kernel.Bind<IRepository>().To<Repository.Repository>().InRequestScope();
+            kernel.Bind<ISettingService>().To<SettingService>().InRequestScope();
+            kernel.Bind<IUserService>().To<UserService>().InRequestScope();
+            kernel.Bind<IFileService>().To<FileService>().InRequestScope();
+            kernel.Bind<AuthorizationRoot>().To<AuthorizationRoot>().InRequestScope();
+
+            kernel.Bind<T034.Core.Services.Administration.IUserService>().To<T034.Core.Services.Administration.UserService>().InRequestScope();
+            kernel.Bind<T034.Core.Services.Administration.IRoleService>().To<T034.Core.Services.Administration.RoleService>().InRequestScope();
+            kernel.Bind<IMenuItemService>().To<MenuItemService>().InRequestScope();
+            kernel.Bind<INewslineService>().To<NewslineService>().InRequestScope();
+
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["WingtipToys"].ConnectionString;
+        }
+
+        private static string ConnectionString { get { return System.Configuration.ConfigurationManager.ConnectionStrings["DatabaseFile"].ConnectionString; } }
+
     }
 }
