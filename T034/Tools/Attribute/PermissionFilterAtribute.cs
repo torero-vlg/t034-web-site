4cfc3a5a4658299fcfd6e678ef742affbe3ad344
Artem <artem.v.rusanov@gmail.com>
2022-02-09 21:07:46 +0300

    t-29 fix PermissionFilterAttribute

--- T034/Tools/Attribute/PermissionFilterAtribute.cs
+++ T034/Tools/Attribute/PermissionFilterAtribute.cs
@@ -12,14 +12,17 @@ namespace T034.Tools.Attribute
 
         public override void OnActionExecuting(Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext filterContext)
         {
-            var action = filterContext.ActionDescriptor.ActionName;
-            var controller = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            //TODO get controllerName action
+            //var controller = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            var controller = "";
+
+            var action = filterContext.ActionDescriptor.DisplayName;
 
             var role = MvcApplication.ActionRoles.FirstOrDefault(p => p.Action == action.ToLower() && p.Controller == controller.ToLower());
             if (role == null) return;
 
-            var rolesCookie = filterContext.RequestContext.HttpContext.Request.Cookies["roles"];
-            if (rolesCookie != null && rolesCookie.Value != null && rolesCookie.Value.Contains(role.Role))
+            var rolesCookie = filterContext.HttpContext.Request.Cookies["roles"];
+            if (rolesCookie != null && rolesCookie != null && rolesCookie.Contains(role.Role))
                 return;
             
             filterContext.Result = new Microsoft.AspNetCore.Mvc.RedirectResult("~/Errors/Unauthorized");
