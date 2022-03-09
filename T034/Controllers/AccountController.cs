6cc7963b8ef32ecd4023bcf81db7bf55445ea666
Artem Rusanov <artem.rusanov@danfoss.com>
2022-03-09 21:39:51 +0300

    t-29 remove oauth2

--- T034/Controllers/AccountController.cs
+++ T034/Controllers/AccountController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using T034.Core.Entity.Administration;
+﻿using System.Collections.Generic;
 using OAuth2;
-using OAuth2.Models;
 using T034.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using T034.Core.DataAccess;
@@ -14,7 +9,7 @@ namespace T034.Controllers
 
     public class AccountController : BaseController
     {
-        public AccountController(AuthorizationRoot authorizationRoot, IBaseDb db) : base(authorizationRoot, db)
+        public AccountController(IBaseDb db) : base(db)
         {
         }
 
@@ -23,110 +18,10 @@ namespace T034.Controllers
         /// </summary>
         public ActionResult Logon(LogonViewModel model)
         {
-            try
-            {
-                var clients = AuthorizationRoot.Clients.Select(client => new LoginInfoModel
-                {
-                    ProviderName = client.Name
-                });
-                model.Clients = clients;
-                return View(model);
-            }
-            catch (Exception ex)
-            {
-                model.Clients = new List<LoginInfoModel>();
-                return View(model);
-            }
-            
+           //TODO fill auth clients
+            model.Clients = new List<LoginInfoModel>();
+            return View(model);
         }
 
-        /// <summary>
-        /// Redirect to login url of selected provider.
-        /// </summary>        
-        public RedirectResult Login(string providerName)
-        {
-            ProviderName = providerName;
-            return new RedirectResult(GetClient().GetLoginLinkUri());
-        }
-
-        /// <summary>
-        /// Renders information received from authentication service.
-        /// </summary>
-        public ActionResult Auth()
-        {
-            try
-            {
-                Logger.Trace(HttpContext.Request.Query["code"]);
-                HttpContext.Response.Cookies.Append("auth_code",
-                   HttpContext.Request.Query["code"],
-                   new Microsoft.AspNetCore.Http.CookieOptions
-                   {
-                       Expires = DateTime.Now.AddDays(30)
-                   });
-
-                Logger.Trace($"Куки auth_code установлены: HttpContext.Request.Query['code']={HttpContext.Request.Query["code"]}, HttpContext.Response.Cookies={HttpContext.Response.Cookies}");
-
-                Logger.Trace($"Получаем информацию о пользователе. Request.QueryString: {Request.QueryString}.");
-                var client = GetClient();
-                Logger.Trace($"Cервис авторизации: {client}");
-
-                var nameValueCollection = new NameValueCollection();
-                foreach (var key in Request.Query.Keys)
-                    nameValueCollection.Add(key, Request.Query[key]);
-
-                UserInfo = client?.GetUserInfo(nameValueCollection) ?? new UserInfo();
-                Logger.Trace($"Пользователь: {UserInfo.Email}");
-
-                //try
-                //{
-                //    Logger.Trace($"Делаем повторный запрос: {UserInfo.Email}");
-                //    UserInfo = client?.GetUserInfo(Request.QueryString) ?? new UserInfo();
-                //    Logger.Trace($"Пользователь2: {UserInfo.Email}");
-                //}
-                //catch (Exception ex)
-                //{
-                //    Logger.Fatal(ex);
-                //}
-
-                var user = Db.SingleOrDefault<User>(u => u.Email == UserInfo.Email);
-                Logger.Trace($"Пользователь из БД: {user.Email}");
-
-                HttpContext.Response.Cookies.Append("roles",
-                    string.Join(",", user.UserRoles.Select(r => r.Code)),
-                    new Microsoft.AspNetCore.Http.CookieOptions
-                    {
-                        Expires = DateTime.Now.AddDays(30)
-                    });
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-            }
-            return View(UserInfo);
-        }
-
-        /// <summary>
-        /// Renders information received from authentication service.
-        /// </summary>
-        public ActionResult Auth2()
-        {
-            Logger.Trace(Request.Cookies["auth_code"]);
-            var nameValueCollection = new NameValueCollection();
-
-            if (Request.Cookies["auth_code"] != null)
-            {
-                Logger.Trace(Request.Cookies["auth_code"]);
-                nameValueCollection.Add("code", Request.Cookies["auth_code"]);
-            }
-
-            Logger.Trace($"Получаем информацию о пользователе. Request.QueryString: {Request.QueryString}.");
-            var client = GetClient();
-            Logger.Trace($"Cервис авторизации: {client}");
-
-            UserInfo = client?.GetUserInfo(nameValueCollection) ?? new UserInfo();
-            Logger.Trace($"Пользователь: {UserInfo.Email}");
-
-            return View("Auth", UserInfo);
-        }
     }
 }
--- T034/Controllers/BaseController.cs
+++ T034/Controllers/BaseController.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Specialized;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using AutoMapper;
 using T034.Core.DataAccess;
-using Ninject;
 using NLog;
 using OAuth2;
-using OAuth2.Client;
 using OAuth2.Models;
 using T034.Profiles;
-using T034.Repository;
 using System.Text;
 
 namespace T034.Controllers
@@ -19,33 +14,12 @@ namespace T034.Controllers
     {
         protected readonly IBaseDb Db;
 
-        protected readonly AuthorizationRoot AuthorizationRoot;
-
-        private const string ProviderNameKey = "providerName";
-
-        protected string ProviderName
-        {
-            get
-            {
-                HttpContext.Session.TryGetValue(ProviderNameKey, out byte[] providerName);
-                return Encoding.UTF8.GetString(providerName);
-            }
-            set => HttpContext.Session.Set(ProviderNameKey, Encoding.UTF8.GetBytes(value));
-        }
-
         protected readonly Logger Logger = LogManager.GetCurrentClassLogger();
         protected UserInfo UserInfo;
 
-        public BaseController(AuthorizationRoot authorizationRoot,
-            IBaseDb db)
+        public BaseController(IBaseDb db)
         {
-            AuthorizationRoot = authorizationRoot;
             Db = db;
-        }
-
-        protected IClient GetClient()
-        {
-            return AuthorizationRoot.Clients.FirstOrDefault(c => c.Name == ProviderName);
         }
 
         public override void OnActionExecuting(Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext context)
@@ -77,41 +51,6 @@ namespace T034.Controllers
                         Email = Request.Cookies["auth"]
                     };
                     return;
-                }
-
-                var nameValueCollection = new NameValueCollection();
-                
-                if (!string.IsNullOrEmpty(HttpContext.Request.Query["code"]))
-                {
-                    foreach(var key in Request.Query.Keys)
-                        nameValueCollection.Add(key, Request.Query[key]);
-
-                    Logger.Trace($"nameValueCollection заполняем из Request.QueryString[code].");
-                }
-                else
-                {
-                    var authCodeCookie = Request.Cookies["auth_code"];
-                    if (authCodeCookie != null)
-                    {
-                        Logger.Trace($"Устанавливаем code: {authCodeCookie}.");
-                        nameValueCollection.Add("code", authCodeCookie);
-                    }
-                }
-
-                Logger.Trace($"nameValueCollection: {nameValueCollection}.");
-                var client = GetClient();
-                Logger.Trace($"Cервис авторизации: {client}. ProviderName:{ProviderName}.");
-
-                var userInfo = client?.GetUserInfo(nameValueCollection);
-                if (userInfo != null)
-                {
-                    Logger.Trace($"Cервис авторизации: {client.Name}. Пользователь: {userInfo.Email}.");
-                    UserInfo = userInfo;
-                }
-                else
-                {
-                    Logger.Trace("Не удалось получить пользователя.");
-                    UserInfo = new UserInfo();
                 }
             }
             catch (Exception ex)
